bd6aa86da132256fbc255ac2b90566c0af24b794
Hasan A Yousef <hasan.ajsf@gmail.com>
2015-12-13 16:19:12 +0300

    node server and APIs

---
+++ server/README.md
@@ -0,0 +1,6 @@
+# myFonix
+
+#App Discription:
+I'm using here voice APIs, to be able to run them, the files should be hosted, so, the app here is a CHROME extension.
+
+The library folder contains the two main scrips, that understand the command (written or spoken)m and speak the answer.
---
+++ server/index.js
@@ -0,0 +1,44 @@
+var server = require('http').createServer(function(request, response) {
+                console.log(request.url);
+                
+     // If external connections allowed, need to overcome the CORS (Cross Origin Resource Sharing) policy //
+   	// Website you wish to allow to connect
+	  response.setHeader('Access-Control-Allow-Origin', '*');
+
+	  // Request methods you wish to allow
+	  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+	  // Request headers you wish to allow
+	  response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+	  // Set to true if you need the website to include cookies in the requests sent
+	  // to the API (e.g. in case you use sessions)
+	  response.setHeader('Access-Control-Allow-Credentials', true);
+  /*  
+        switch (request.url.indexOf("api")){
+        case 1:
+              console.log('api');
+              serveAPI(request, response);
+        break;
+        default:
+              serveHTML(request, response);
+        break;           
+        } */
+  })
+  , WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({ server: server })
+  , host = '172.0.0.1'
+  , port = 8125
+  
+//  , serveHTML = require("./Modules/staticServer.js")
+//  , serveAPI  = require("./Modules/apiServer.js")
+  ;
+ 
+wss.on('connection', function(ws) {
+    ws.on('message', function(message) {
+        console.log('received: %s', message);
+    });
+    ws.send('something');
+});
+ 
+server.listen(port, function () { console.log('Listening on ' + server.address().port) });
